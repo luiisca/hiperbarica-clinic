d786b5136e5b7fb00c48badb0d4f00a6aa673fcf
Luis Cadillo <luiscaf3r@gmail.com>
2023-04-14 22:44:40 -0500

    ⚡add next fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,30 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Inter, Lora } from "next/font/google";
+
+// config Inter and Lora fonts from Google Fonts with the latin subset
+const InterFont = Inter({
+  subsets: ["latin"],
+  variable: "--inter-font",
+});
+const LoraFont = Lora({
+  subsets: ["latin"],
+  variable: "--loar-font",
+});
 
 import "src/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --inter-font: ${InterFont.variable};
+          --lora-font: ${LoraFont.variable};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,28 @@
-import { type Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#f1f9ff",
+          200: "#e1f3ff",
+          400: "#74c0fc",
+          500: "#3f9cf5",
+        },
+        gray: {
+          100: "#767676",
+          200: "#555",
+          500: "#333",
+        },
+        // gainsboro: "#ddd",
+      },
+      fontFamily: {
+        inter: ["var(--inter-font)", "sans-serif"],
+        lora: ["var(--lora-font)", "serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
