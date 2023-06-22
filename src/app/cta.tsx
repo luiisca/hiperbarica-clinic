2aca9f517421f232b8f1e0d0a29ab9b6aced7ab9
Luis Cadillo <luiscaf3r@gmail.com>
2023-06-22 13:27:24 -0500

    fix: <Cta />  padding

--- src/app/cta.tsx
+++ src/app/cta.tsx
@@ -2,45 +2,32 @@ import { Button } from "@/components/ui/core/button";
 import Heading from "@/components/ui/core/heading";
 import { cn } from "@/utils/cn";
 
-const Container = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => {
-  return (
-    <div className={cn("mx-auto max-w-screen-xl px-8 py-24", className)}>
-      {props.children}
-    </div>
-  );
-};
-
 export default function Cta(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <section {...props} className={cn("w-full", props.className)}>
-      <Container className="px-8">
-        <div className="mx-auto w-11/12 rounded-3xl bg-primary-200 py-24 xl:w-full">
-          <div
-            id="cta"
-            data-intersect="false"
-            className={cn(
-              "flex flex-col items-center justify-center space-y-10 text-center md:space-y-12",
-              "data-[intersect=false]:opacity-0 data-[intersect=true]:animate-in data-[intersect=true]:fade-in data-[intersect=true]:slide-in-from-bottom-16 data-[intersect=true]:duration-1000"
-            )}
-          >
-            <div className="flex max-w-prose flex-col items-center">
-              <Heading type="secondary" className="mb-4 md:mb-8">
-                ¿Listo para mejorar tu salud con la terapia hiperbárica? ¡Agenda
-                una cita hoy mismo!
-              </Heading>
-              <p className="max-w-lg">
-                ¡No esperes más y comienza tu camino hacia una mejor salud con
-                Hiperbárica del Sur Perú! Nuestro equipo médico profesional
-                estará encantado de atenderte y responder a todas tus preguntas
-              </p>
-            </div>
-            <Button href="/citas">Agendar cita</Button>
+      <div className="mx-auto w-11/12 rounded-3xl bg-primary-200 px-8 py-24 xl:mx-auto xl:max-w-screen-xl 2xl:w-full">
+        <div
+          id="cta"
+          data-intersect="false"
+          className={cn(
+            "flex flex-col items-center justify-center space-y-10 text-center md:space-y-12",
+            "data-[intersect=false]:opacity-0 data-[intersect=true]:animate-in data-[intersect=true]:fade-in data-[intersect=true]:slide-in-from-bottom-16 data-[intersect=true]:duration-1000"
+          )}
+        >
+          <div className="flex max-w-prose flex-col items-center">
+            <Heading type="secondary" className="mb-4 md:mb-8">
+              ¿Listo para mejorar tu salud con la terapia hiperbárica? ¡Agenda
+              una cita hoy mismo!
+            </Heading>
+            <p className="max-w-lg">
+              ¡No esperes más y comienza tu camino hacia una mejor salud con
+              Hiperbárica del Sur Perú! Nuestro equipo médico profesional estará
+              encantado de atenderte y responder a todas tus preguntas
+            </p>
           </div>
+          <Button href="/citas">Agendar cita</Button>
         </div>
-      </Container>
+      </div>
     </section>
   );
 }
