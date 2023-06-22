8aa6773cfe5f915b0c63242ec4f2a0d9ddf5ed34
Luis Cadillo <luiscaf3r@gmail.com>
2023-06-22 13:11:29 -0500

    fix: <Spinner /> viewbox

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -12,7 +12,7 @@ export default function Spinner({
           "h-6 w-6 animate-spin fill-primary-500 text-primary-100",
           className
         )}
-        viewBox="0 0 50 50"
+        viewBox="0 0 100 100"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
